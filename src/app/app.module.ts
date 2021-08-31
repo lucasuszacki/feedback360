073f62971fd40d471c6f1fd4b3b8042f3961c027
Lucas Uszacki <lucas.uszacki@acad.pucrs.br>
2021-08-31 00:25:17 -0300

    chore: add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
